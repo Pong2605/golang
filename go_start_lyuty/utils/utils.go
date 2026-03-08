06cd2bab565f9e2d09835aad2ab24be6b5439f2c
Kucherov Nikita <kucher2605@gmail.com>
2026-03-08 13:52:52 +0300

    updated

--- go_start_lyuty/utils/utils.go
+++ go_start_lyuty/utils/utils.go
@@ -17,3 +17,47 @@ func Out(from, to int) {
     fmt.Println(i)
   }
 }
+
+func OutCh(from, to int, ch chan bool) {
+  for i := from; i <= to; i++ {
+      time.Sleep(50 * time.Millisecond)
+      fmt.Println(i)
+  }
+  ch <- true
+}
+
+// Как видите, наши функции передают результат в каналы. 
+// Теперь мы можем вызвать их как горутины в main() и вывести полученную сумму.
+
+// Мы используем каналы для получения результата каждой горутины и вывода их суммы.
+
+// Если вам больше не нужно отправлять данные в канал, вы можете закрыть его, используя close(ch), 
+// где ch - имя канала. Это делается в отправителе.
+
+func EvenSum(from, to int, ch chan int) {
+  result := 0
+  for i := from; i <= to; i++ {
+    if i % 2 == 0 {
+      result += i
+    }    
+  }
+  ch <- result
+}
+
+func SquareSum(from, to int, ch chan int) {
+  result := 0
+  for i := from; i <= to; i++ {
+    if i % 2 == 0 {
+      result += i*i
+    }    
+  }
+  ch <- result
+}
+
+func Download(size int, ch chan int) {
+  result := 0
+  for i := 0; i <= size; i++ {
+      result += i
+  }
+  ch <- result
+}
